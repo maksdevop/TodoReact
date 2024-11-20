674099f8c12859ebcd73755e04a6c817615c03aa
maksdevop <maksmm.sirotin@mail.ru>
2024-11-20 22:27:49 +0300

    Updates

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'airbnb',
+        'prettier',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        parser: 'babel-eslint',
+    },
+    plugins: ['react', 'react-hooks', 'prettier', '@typescript-eslint'],
+    rules: {
+        'prettier/prettier': 'warn',
+        'react/react-in-jsx-scope': 'off',
+        'react/prop-types': 'off',
+        'jsx-a11y/no-autofocus': 'off',
+        'jsx-a11y/control-has-associated-label': 'off',
+        'jsx-a11y/label-has-associated-control': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'jsx-a11y/no-static-element-interactions': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'import/no-extraneous-dependencies': [
+            'error',
+            { devDependencies: true },
+        ],
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+            },
+        ],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx'],
+            },
+        },
+    },
+};
